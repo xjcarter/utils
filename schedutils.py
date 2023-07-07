5571cd71fcc52ad652254e1a41b3f54bf3b6caef
James Carter <xjcarter@gmail.com>
2023-07-07 09:16:12 -0400

    add scheduling util module

---
+++ schedutils.py
@@ -0,0 +1,16 @@
+
+## create date time object a from time string (HH:MM)
+## calculate the second between a benchmark datetime and the forward looking time.
+def seconds_util(benchmark, time_string):
+    now = benchmark
+    current_date = now.date()  # Get the current date
+    time_parts = time_string.split(':')
+
+    hour = int(time_parts[0])
+    minute = int(time_parts[1])
+
+    # Create a datetime object using the current date and the provided time
+    new_time = datetime.combine(current_date, datetime.min.time()) + timedelta(hours=hour, minutes=minute)
+    secs_util = (new_time - now).total_seconds()
+
+    return new_time, secs_util
